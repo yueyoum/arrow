efe7c9627f8e394393d6416d7b70d3eb889b4ffe
yueyoum <yueyoum@gmail.com>
2015-04-08 12:59:28 +0800

    adjust -spec

--- src/arrow.erl
+++ src/arrow.erl
@@ -32,37 +32,43 @@
 -type arrow_range() :: {arrow_datetime(), arrow_datetime()}.
 -type arrow_compare() :: -1 | 0 | 1.
 
+-export_type([arrow_datetime/0, arrow_range/0, arrow_compare/0]).
+
 -define(BASE_SECONDS, 62167219200).
 
 %% @doc
 %% Get Now with format `{MegaSecs, Secs, MicroSecs}'
 %% @end
--spec now() -> erlang:timestamp().
+-spec now() -> Return when
+        Return  :: erlang:timestamp().
 now() ->
     os:timestamp().
 
 %% @doc
 %% Get Now timestamp
 %% @end
--spec timestamp() -> integer().
+-spec timestamp() -> UnixTimestamp when
+        UnixTimestamp   :: integer().
 timestamp() ->
     unix_timestamp(arrow:now()).
 
 %% @doc
 %% Get timestamp of given time
 %% @end
--spec timestamp(arrow_datetime()) -> integer().
+-spec timestamp(Input) -> UnixTimestamp when
+        Input           :: arrow_datetime(),
+        UnixTimestamp   :: integer().
 timestamp(Datetime) ->
     unix_timestamp(arrow:get(Datetime)).
 
 %% @doc
 %% Format given time to format `YYYY-MM-DD HH:mm:ss'
 %% @end
--spec format(Timestamp :: erlang:timestamp()) -> DateString :: nonempty_string();
-            (Datetime :: calendar:datetime()) -> DateString :: nonempty_string().
-format(Timestamp) when is_integer(Timestamp)->
-    format(unix_timestamp_to_datetime(Timestamp));
-
+-spec format(Input) -> DateString when
+        Input           :: integer() | calendar:datetime(),
+        DateString      :: nonempty_string().
+format(Input) when is_integer(Input)->
+    format(unix_timestamp_to_datetime(Input));
 format({{Year, Month, Day}, {Hour, Minute, Second}}) ->
     Text = io_lib:fwrite("~4..0b-~2..0b-~2..0b ~2..0b:~2..0b:~2..0b",
         [Year, Month, Day, Hour, Minute, Second]
@@ -73,14 +79,17 @@ format({{Year, Month, Day}, {Hour, Minute, Second}}) ->
 %% @doc
 %% Get Current time as format `{{Year, Month, Day}, {Hour, Minute, Second}}'
 %% @end
--spec get() -> calendar:datetime().
+-spec get() -> Datetime when
+        Datetime    :: calendar:datetime().
 get() ->
     calendar:universal_time().
 
 %% @doc
 %% Parse given time to format `{{Year, Month, Day}, {Hour, Minute, Second}}'
 %% @end
--spec get(arrow_datetime()) -> calendar:datetime().
+-spec get(Input) -> Datetime when
+        Input       :: arrow_datetime(),
+        Datetime    :: calendar:datetime().
 get(Timestamp) when is_integer(Timestamp) ->
     unix_timestamp_to_datetime(Timestamp);
 
@@ -97,7 +106,10 @@ get({{_, _, _}, {_, _, _}} = Datetime) ->
 %% @doc
 %% Diff seconds of D1 and D2. `D1 - D2'
 %% @end
--spec diff(D1 :: arrow_datetime(), D2 :: arrow_datetime()) -> DiffSeconds :: integer().
+-spec diff(D1, D2) -> DiffSeconds when
+        D1              :: arrow_datetime(),
+        D2              :: arrow_datetime(),
+        DiffSeconds     :: integer().
 diff(D1, D2) ->
     D1Seconds = timestamp(D1),
     D2Seconds = timestamp(D2),
@@ -107,7 +119,10 @@ diff(D1, D2) ->
 %% @doc
 %% Compare D1 and D2.
 %% @end
--spec compare(D1 :: arrow_datetime(), D2 :: arrow_datetime()) -> Result :: arrow_compare().
+-spec compare(D1, D2) -> CompareResult when
+        D1              :: arrow_datetime(),
+        D2              :: arrow_datetime(),
+        CompareResult   :: arrow_compare().
 compare(D1, D2) ->
     case diff(D1, D2) of
         N when N == 0 -> 0;
@@ -118,7 +133,10 @@ compare(D1, D2) ->
 %% @doc
 %% Check whether D2 or D2Range is in D1Range.
 %% @end
--spec in(arrow_range(), arrow_datetime()) -> boolean().
+-spec in(D1Range, D2) -> Result when
+        D1Range         :: arrow_range(),
+        D2              :: arrow_datetime(),
+        Result          :: boolean().
 in({D1Start, D1End}, D2) ->
     CompareWithStart = compare(D2, D1Start),
     ComareWithEnd = compare(D2, D1End),
@@ -128,7 +146,10 @@ in({D1Start, D1End}, D2) ->
 %% @doc
 %% Add Years to Input Datetime
 %% @end
--spec add_years(arrow_datetime(), integer()) -> calendar:datetime().
+-spec add_years(Input, Years) -> Datetime when
+        Input           :: arrow_datetime(),
+        Years           :: integer(),
+        Datetime        :: calendar:datetime().
 add_years(Datetime, Years) ->
     {{Year, Month, Day}, Time} = arrow:get(Datetime),
     {{Year + Years, Month, Day}, Time}.
@@ -136,7 +157,10 @@ add_years(Datetime, Years) ->
 %% @doc
 %% Add Months to Input Datetime
 %% @end
--spec add_months(arrow_datetime(), integer()) -> calendar:datetime().
+-spec add_months(Input, Months) -> Datetime when
+        Input           :: arrow_datetime(),
+        Months          :: integer(),
+        Datetime        :: calendar:datetime().
 add_months(Datetime, Months) ->
     {{Year, Month, _Day}, _Time} = arrow:get(Datetime),
     AddDay = do_add_months(Months, 0, {Year, Month, 0}),
@@ -145,7 +169,10 @@ add_months(Datetime, Months) ->
 %% @doc
 %% Add Days to Input Datetime
 %% @end
--spec add_days(arrow_datetime(), integer()) -> calendar:datetime().
+-spec add_days(Input, Days) -> Datetime when
+        Input           :: arrow_datetime(),
+        Days            :: integer(),
+        Datetime        :: calendar:datetime().
 add_days(Datetime, Days) ->
     {Date, Time} = arrow:get(Datetime),
     TotalDays = calendar:date_to_gregorian_days(Date) + Days,
@@ -155,43 +182,61 @@ add_days(Datetime, Days) ->
 %% @doc
 %% Add Hours to Input Datetime
 %% @end
--spec add_hours(arrow_datetime(), integer()) -> calendar:datetime().
+-spec add_hours(Input, Hours) -> Datetime when
+        Input           :: arrow_datetime(),
+        Hours           :: integer(),
+        Datetime        :: calendar:datetime().
 add_hours(Datetime, Hours) ->
     add_seconds(Datetime, Hours*3600).
 
 %% @doc
 %% Add Minutes to Input Datetime
 %% @end
--spec add_minutes(arrow_datetime(), integer()) -> calendar:datetime().
+-spec add_minutes(Input, Minutes) -> Datetime when
+        Input           :: arrow_datetime(),
+        Minutes         :: integer(),
+        Datetime        :: calendar:datetime().
 add_minutes(Datetime, Minutes) ->
     add_seconds(Datetime, Minutes*60).
 
 %% @doc
 %% Add Seconds to Input Datetime
 %% @end
--spec add_seconds(arrow_datetime(), integer()) -> calendar:datetime().
-add_seconds(Datetime, AddSecond) ->
+-spec add_seconds(Input, Seconds) -> Datetime when
+        Input           :: arrow_datetime(),
+        Seconds         :: integer(),
+        Datetime        :: calendar:datetime().
+add_seconds(Datetime, Seconds) ->
     DatetimeNormalized = arrow:get(Datetime),
-    TotalSeconds = calendar:datetime_to_gregorian_seconds(DatetimeNormalized) + AddSecond,
+    TotalSeconds = calendar:datetime_to_gregorian_seconds(DatetimeNormalized) + Seconds,
     calendar:gregorian_seconds_to_datetime(TotalSeconds).
 
 %%% ====================================================
 %%% Interal Functions
 %%% ====================================================
 
--spec unix_timestamp(erlang:timestamp()) -> integer();
-                    (calendar:datetime()) -> integer().
+-spec unix_timestamp(Input) -> UnixTimestamp when
+        Input           :: erlang:timestamp() | calendar:datetime(),
+        UnixTimestamp   :: integer().
 unix_timestamp({MegaSecs, Secs, _MicroSecs}) ->
     MegaSecs * 1000000 + Secs;
 
 unix_timestamp(DateTime) ->
     calendar:datetime_to_gregorian_seconds(DateTime) - ?BASE_SECONDS.
 
--spec unix_timestamp_to_datetime(integer()) -> calendar:datetime() .
+-spec unix_timestamp_to_datetime(UnixTimestamp) -> Datetime when
+        UnixTimestamp   :: integer(),
+        Datetime        :: calendar:datetime().
 unix_timestamp_to_datetime(Timestamp) ->
     calendar:gregorian_seconds_to_datetime(Timestamp + ?BASE_SECONDS).
 
-
+-spec do_add_months(AddMonth, Acc, {Year, Month, AddDays}) -> AddDaysResult when
+        AddMonth        :: integer(),
+        Acc             :: integer(),
+        Year            :: integer(),
+        Month           :: 1..12,
+        AddDays         :: integer(),
+        AddDaysResult   :: integer().
 do_add_months(AddMonth, Acc, {_, _, AddDays}) when AddMonth == Acc ->
     AddDays;
 
